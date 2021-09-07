3cb27f6d14f2fdb0b2ee1573dd89c6e984005982
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-09-08 05:39:41 +0900

    add 메뉴리뉴얼.js

---
+++ 메뉴리뉴얼.js
@@ -0,0 +1,59 @@
+function solution(orders, course) {
+  const result = [];
+  const answer = Array.from(Array(11), () => []);
+
+  const createPowerSet = (order) => {
+    const res = [];
+    const dfs = (start = 0, arr = []) => {
+      res.push([arr.sort().join("")]);
+
+      for (let i = start; i < order.length; i++) {
+        dfs(i + 1, [...arr, order[i]]);
+      }
+    };
+    dfs();
+    return res;
+  };
+
+  orders
+    .map((order) => createPowerSet(order))
+    .map((elem) => {
+      const ans = elem.filter((e) => course.includes(e[0].length));
+      return ans;
+    })
+    .map((elem) => {
+      const ans = elem.map((e) => e.concat(1));
+      return ans;
+    })
+    .forEach((elem) => {
+      elem.forEach((e) => {
+        const FindElem = result.find((f) => f[0] === e[0]);
+        if (FindElem) {
+          FindElem[1] += 1;
+        } else {
+          result.push(e);
+        }
+      });
+    });
+
+  result.forEach((elem) => {
+    answer[elem[0].length].push([elem]);
+  });
+
+  const Realresult = answer.map((e) => {
+    return e
+      .sort((a, b) => b[0][1] - a[0][1])
+      .filter((f) => f[0][1] > 1)
+      .filter((f, idx, array) => {
+        return array[0][0][1] === f[0][1];
+      });
+  });
+  const realAnswer = [];
+  course.forEach((i) => {
+    Realresult[i].forEach((e) => {
+      realAnswer.push(e[0][0]);
+    });
+  });
+
+  return realAnswer.sort((a, b) => a.localeCompare(b));
+}
