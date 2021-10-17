b4b1f0487e8b91d4b396783d10894b6ebfea3422
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-10-17 20:57:01 +0900

    add 숫자문자열.js

---
+++ .history/숫자문자열_20211017205205.js
@@ -0,0 +1,45 @@
+const checkNumber = /[0-9]/;
+const s = "one4seveneight";
+const convertStringToNumber = (str) => {
+  switch (str) {
+    case "zero":
+      return "0";
+    case "one":
+      return "1";
+    case "two":
+      return "2";
+    case "three":
+      return "3";
+    case "four":
+      return "4";
+    case "five":
+      return "5";
+    case "six":
+      return "6";
+    case "seven":
+      return "7";
+    case "eight":
+      return "8";
+    case "nine":
+      return "9";
+    default:
+      return "";
+  }
+};
+
+function solution(s) {
+  var answer = "";
+  let save = "";
+  Array.from(s).forEach((e) => {
+    if (e.match(checkNumber)) {
+      answer += convertStringToNumber(save);
+      save = "";
+      answer += e;
+    } else {
+      save += e;
+    }
+  });
+  return answer;
+}
+
+console.log(solution(s));
---
+++ .history/숫자문자열_20211017205448.js
@@ -0,0 +1,48 @@
+const checkNumber = /[0-9]/;
+const s = "23four5six7";
+const convertStringToNumber = (str) => {
+  switch (str) {
+    case "zero":
+      return "0";
+    case "one":
+      return "1";
+    case "two":
+      return "2";
+    case "three":
+      return "3";
+    case "four":
+      return "4";
+    case "five":
+      return "5";
+    case "six":
+      return "6";
+    case "seven":
+      return "7";
+    case "eight":
+      return "8";
+    case "nine":
+      return "9";
+    default:
+      return "";
+  }
+};
+
+function solution(s) {
+  var answer = "";
+  let save = "";
+  Array.from(s).forEach((e) => {
+    if (e.match(checkNumber)) {
+      answer += e;
+    } else {
+      save += e;
+      addValue = convertStringToNumber(save);
+      if (addValue !== "") {
+        answer += addValue;
+        save = "";
+      }
+    }
+  });
+  return answer;
+}
+
+console.log(solution(s));
