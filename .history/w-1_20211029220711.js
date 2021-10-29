b4d91b137bb5fdebfb1fc14d2f3a97859761ae7f
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-10-29 22:07:37 +0900

    add w-1.js

---
+++ .history/w-1_20211029220711.js
@@ -0,0 +1,21 @@
+const calculate = (answer, money, value, cnt) => {
+  answer[cnt] += Math.floor(money / value);
+  return money % value;
+};
+
+const solution = (money) => {
+  const answer = new Array(9).fill(0);
+
+  money = calculate(answer, money, 50000, 0);
+  money = calculate(answer, money, 10000, 1);
+  money = calculate(answer, money, 5000, 2);
+  money = calculate(answer, money, 1000, 3);
+  money = calculate(answer, money, 500, 4);
+  money = calculate(answer, money, 100, 5);
+  money = calculate(answer, money, 50, 6);
+  money = calculate(answer, money, 10, 7);
+  money = calculate(answer, money, 1, 8);
+  console.log(answer);
+};
+
+solution(15000);
