cd5f18bcf6f0d187744a1b2556c41baeff3686eb
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-11-04 01:27:37 +0900

    add 카펫

---
+++ .history/문자열_20211104012556.js
@@ -0,0 +1,16 @@
+function solution(brown, yellow) {
+  for (let i = 3; i < Math.floor(brown / 2); i++) {
+    if (yellow % (i - 2) === 0) {
+      const j = yellow / (i - 2) + 2;
+      if (brown + yellow === i * j) {
+        if (i >= j) {
+          return [i, j];
+        } else {
+          return [j, i];
+        }
+      }
+    }
+  }
+}
+
+console.log(solution(8, 1));
